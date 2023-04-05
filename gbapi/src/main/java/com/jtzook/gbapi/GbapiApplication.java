3f6f87c4ff9b26cb393a7a3b90d9c660498d8d9a
Jared Zook <jaredzook@gmail.com>
2023-04-05 16:50:59 -0700

    add Spring Boot App

---
+++ gbapi/src/main/java/com/jtzook/gbapi/GbapiApplication.java
@@ -0,0 +1,13 @@
+package com.jtzook.gbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbapiApplication.class, args);
+	}
+
+}
---
+++ gbapi/src/test/java/com/jtzook/gbapi/GbapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtzook.gbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
