b0927ffd287f5f502092bae44e6e7e5d6dcd96b6
Jared Zook <7940096+jtzook@users.noreply.github.com>
2023-06-14 15:23:42 -0700

    Implement User

--- gbapi/src/main/java/com/jtzook/gbapi/models/User.java
+++ gbapi/src/main/java/com/jtzook/gbapi/models/User.java
@@ -1,72 +1,67 @@
 package com.jtzook.gbapi.models;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import jakarta.persistence.*; // for Spring Boot 3
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Collection;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.io.Serializable;
 
-public class User implements UserDetails {
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
 
+    private static PasswordEncoder passwordEncoder;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false)
     private String username;
+
+    @Column(nullable = false)
     private String password;
-    private Set<String> roles;
+
+    @Column(unique = true, nullable = false)
+    private String email;
 
     public User() {
     }
 
-    public User(String username, String password, Set<String> roles) {
+    public User(String username, String password, String email) {
         this.username = username;
-        this.password = password;
-        this.roles = roles;
+        this.password = passwordEncoder.encode(password);
+        this.email = email;
     }
 
     public Long getId() {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getUsername() {
         return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public Set<String> getRoles() {
-        return roles;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    // Implement UserDetails methods
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        // Convert roles to GrantedAuthority objects
-        return roles.stream()
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
+    public String getEmail() {
+        return email;
     }
 
-    @Override
-    public boolean isEnabled() {
-        return true; // Modify the logic as needed
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true; // Modify the logic as needed
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true; // Modify the logic as needed
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true; // Modify the logic as needed
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
