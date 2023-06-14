b143f30a066ce695cac05058f30b7e34774ba173
Jared Zook <7940096+jtzook@users.noreply.github.com>
2023-06-14 15:28:58 -0700

    add UserRepository

--- gbapi/src/main/java/com/jtzook/gbapi/repositories/UserRepository.java
+++ gbapi/src/main/java/com/jtzook/gbapi/repositories/UserRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.jtzook.gbapi.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     User findByUsername(String username);
+
+    User findByEmail(String email);
 }
 
