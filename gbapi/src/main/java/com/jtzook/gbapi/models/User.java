9e0b89164c730b71d579463091377916a89aa365
Jared Zook <7940096+jtzook@users.noreply.github.com>
2023-06-14 16:24:31 -0700

    add User

--- gbapi/src/main/java/com/jtzook/gbapi/models/User.java
+++ gbapi/src/main/java/com/jtzook/gbapi/models/User.java
@@ -1,67 +1,25 @@
 package com.jtzook.gbapi.models;
 
-import jakarta.persistence.*; // for Spring Boot 3
-import org.springframework.security.crypto.password.PasswordEncoder;
+import java.util.ArrayList;
+import java.util.Collection;
 
-import java.io.Serializable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "users")
-public class User implements Serializable {
-
-    private static PasswordEncoder passwordEncoder;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+@Entity @Data @NoArgsConstructor @AllArgsConstructor
+public class User {
+    @Id @GeneratedValue(strategy = jakarta.persistence.GenerationType.AUTO)
     private Long id;
-
-    @Column(unique = true, nullable = false)
+    private String name;
     private String username;
-
-    @Column(nullable = false)
     private String password;
 
-    @Column(unique = true, nullable = false)
-    private String email;
-
-    public User() {
-    }
-
-    public User(String username, String password, String email) {
-        this.username = username;
-        this.password = passwordEncoder.encode(password);
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> roles = new ArrayList<>();
 }
