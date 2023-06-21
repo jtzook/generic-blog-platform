7599484fb2e1f2ec782a43e7f0562171471c7bb1
Jared Zook <7940096+jtzook@users.noreply.github.com>
2023-06-21 12:39:17 -0700

    add user repository and details

--- gbapi/src/main/java/com/jtzook/gbapi/models/User.java
+++ gbapi/src/main/java/com/jtzook/gbapi/models/User.java
@@ -9,7 +9,7 @@ import java.util.Set;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     private String username;
 
@@ -22,4 +22,36 @@ public class User {
     private Set<Role> roles = new HashSet<>();
 
     // getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
