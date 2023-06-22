4affd9fc59ef02a454f32ab1c706e0f515d3f18e
Jared Zook <7940096+jtzook@users.noreply.github.com>
2023-06-21 17:22:55 -0700

    make auth filter a component

--- gbapi/src/main/java/com/jtzook/gbapi/auth/JwtAuthTokenFilter.java
+++ gbapi/src/main/java/com/jtzook/gbapi/auth/JwtAuthTokenFilter.java
@@ -18,6 +18,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class JwtAuthTokenFilter extends OncePerRequestFilter {
 
     @Autowired
