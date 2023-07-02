c385de373901beda97ff9bb080b153d2375098a6
Jared Zook <7940096+jtzook@users.noreply.github.com>
2023-07-02 15:02:16 -0700

    configure appshell

--- web/src/components/layout.tsx
+++ web/src/components/layout.tsx
@@ -1,32 +1,85 @@
-import type { ReactNode } from "react";
-import { AppShell, Navbar, Header } from "@mantine/core";
+import { ReactNode, useState } from "react"
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Footer,
+  Aside,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+} from "@mantine/core"
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const theme = useMantineTheme()
+  const [opened, setOpened] = useState(false)
   return (
     <AppShell
-      padding="md"
-      navbar={
-        <Navbar width={{ base: 300 }} height={500} p="xs">
-          {/* Navbar content */}
-        </Navbar>
-      }
-      header={
-        <Header height={60} p="xs">
-          {/* Header content */}
-        </Header>
-      }
-      styles={(theme) => ({
+      styles={{
         main: {
-          backgroundColor:
+          background:
             theme.colorScheme === "dark"
               ? theme.colors.dark[8]
               : theme.colors.gray[0],
         },
-      })}
+      }}
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      // navbar={
+      //   <Navbar
+      //     p="md"
+      //     hiddenBreakpoint="sm"
+      //     hidden={!opened}
+      //     width={{ sm: 200, lg: 300 }}
+      //   >
+      //     <Text>Application navbar</Text>
+      //   </Navbar>
+      // }
+      // aside={
+      //   <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
+      //     <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
+      //       <Text>Application sidebar</Text>
+      //     </Aside>
+      //   </MediaQuery>
+      // }
+      footer={
+        <Footer height={{ base: 50, md: 70 }} p="md">
+          {" "}
+        </Footer>
+      }
+      header={
+        <Header height={{ base: 50, md: 70 }} p="md">
+          <div
+            style={{ display: "flex", alignItems: "center", height: "100%" }}
+          >
+            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+
+            <Text
+              variant="gradient"
+              gradient={{ from: "indigo", to: "cyan", deg: 45 }}
+              sx={{ fontFamily: "Greycliff CF, sans-serif" }}
+              ta="center"
+              fz="lg"
+              fw={700}
+            >
+              GENERIC BLOG PLATFORM
+            </Text>
+          </div>
+        </Header>
+      }
     >
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex flex-col items-center justify-between p-24">
         {children}
       </main>
     </AppShell>
-  );
+  )
 }
