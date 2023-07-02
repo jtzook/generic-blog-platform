1d72a0ceb9f27c06df010887b28aefe9bf0354b5
Jared Zook <7940096+jtzook@users.noreply.github.com>
2023-07-02 14:51:32 -0700

    AppShell

--- web/src/components/layout.tsx
+++ web/src/components/layout.tsx
@@ -1,9 +1,32 @@
-import type { ReactNode } from "react"
+import type { ReactNode } from "react";
+import { AppShell, Navbar, Header } from "@mantine/core";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <AppShell
+      padding="md"
+      navbar={
+        <Navbar width={{ base: 300 }} height={500} p="xs">
+          {/* Navbar content */}
+        </Navbar>
+      }
+      header={
+        <Header height={60} p="xs">
+          {/* Header content */}
+        </Header>
+      }
+      styles={(theme) => ({
+        main: {
+          backgroundColor:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        },
+      })}
+    >
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
         {children}
-    </main>
-  )
+      </main>
+    </AppShell>
+  );
 }
