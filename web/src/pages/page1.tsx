f1b2e879e4398d84e6ce79c1f64bce4345714b02
Jared Zook <7940096+jtzook@users.noreply.github.com>
2023-07-16 15:22:25 -0700

    routing example

---
+++ web/src/pages/page1.tsx
@@ -0,0 +1,5 @@
+import Layout from '@/components/layout'
+
+export default function Page1() {
+  return <Layout>Page 1</Layout>
+}
---
+++ web/src/pages/page2.tsx
@@ -0,0 +1,5 @@
+import Layout from '@/components/layout'
+
+export default function Page2() {
+  return <Layout>Page 2</Layout>
+}
